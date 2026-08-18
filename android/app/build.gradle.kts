dff8a57c5c995febe6cfe7f854f20534ffe5641d
blackpirateapps <github@blackpiratex.com>
2026-08-18 19:53:52 +0530

    fix(android): configure compileSdk 36 for app and subprojects to satisfy lifecycle AAR metadata

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.blackpiratex.quietpaper"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
